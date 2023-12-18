12af0296ba74a738477d7898f06feccdbc64c359
Aditya443 <96006507+HakiruKamiZX@users.noreply.github.com>
2023-12-18 23:20:21 +0700

    Commit answer for quiz

---
+++ main.jl
@@ -0,0 +1,88 @@
+using Serialization
+using DataFrames
+using LinearAlgebra
+
+
+#Mean dengan euclidean
+function euclidean_mean(data_frame)
+    means = Float64[]
+    for col in eachcol(data_frame)
+        col_values = coalesce.(Float64.(col), 0.0)
+        n = length(col_values)
+        
+        # Menghitung ruclidean per kolom
+        euclidean_mean_val = mean(col_values)
+
+        if n == 0
+            push!(means, NaN)
+        else
+            push!(means, euclidean_mean_val)
+        end
+    end
+    return means
+end
+
+#Membagi data menjadi kelas
+function calculate_class_means(data_frame)
+    unique_classes = unique(data_frame[!, end])
+    means_by_class = Dict{eltype(unique_classes), Vector{Float64}}()
+
+    for class_label in unique_classes
+        class_indices = findall(x -> x == class_label, data_frame[!, end])
+        class_data = data_frame[class_indices, 1:end-1]
+        
+        means = euclidean_mean(class_data)
+        means_by_class[class_label] = means
+    end
+
+    return means_by_class
+end
+
+
+function print_mean_for_class(class_label, means)
+    println("Mean for Class $class_label:")
+    println(means)
+end
+
+#menghitung mean dengan cascade
+function cascade_classify_mean(data_frame, classifier)
+    unique_classes = unique(data_frame[!, end])
+    means_by_class = Dict{eltype(unique_classes), Vector{Float64}}()
+
+    for class_label in unique_classes
+        class_indices = findall(x -> x == class_label, data_frame[!, end])
+        class_data = data_frame[class_indices, 1:end-1]
+        
+        classified_indices = classifier.(eachrow(class_data))
+        classified_data = class_data[classified_indices, :]
+        
+        means = custom_mean(classified_data)
+        means_by_class[class_label] = means
+    end
+
+    return means_by_class
+end
+
+path = "data_9m.mat"
+
+serialized_data = read(path, String)
+
+deserialized_data = deserialize(IOBuffer(serialized_data))
+
+data_matrix = Matrix(deserialized_data)
+
+classes = data_matrix[:, end]
+
+classifier(row) = row[1] < 0 ? true : false
+
+df = DataFrame(data_matrix, :auto)
+
+println("Euclidesn mean result:")
+for (class_label, means) in calculate_class_means(df)
+    print_mean_for_class(class_label, means)
+end
+
+println("\nCascade classify result:")
+for (class_label, means) in cascade_classify_mean(df, classifier)
+    print_mean_for_class(class_label, means)
+end
